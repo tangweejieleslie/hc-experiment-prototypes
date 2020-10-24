3a44a06eda6fd96cceceed0ee08c8d480224e213
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-10-24 21:07:59 +0800

    test code: adding log to button click

--- prototype/src/views/About.vue
+++ prototype/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn
+    @click="endTime"
+    > 
+      endTime
+    </v-btn>
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from "vue";
+// import Home from "./views/Home.vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {
+    // Home
+  },
+
+  data: () => ({
+    //
+  }),
+
+  methods: {
+    endTime: function() {
+      console.log("End: ", Date.now())
+    }
+  }
+});
+</script>
--- prototype/src/views/Home.vue
+++ prototype/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>test</h1>
+    <v-btn @click="endTime">
+      endTime
+    </v-btn>
   </div>
 </template>
 
@@ -12,7 +15,19 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 @Component({
   components: {
     HelloWorld
+  },
+  mounted: function() {
+    console.time("test1");
+    console.log("hi");
+    console.log("Start: ", Date.now())
+  },
+  methods: {
+    endTime: function() {
+      console.log("End: ", Date.now())
+    }
   }
+
+  // time("test1")
 })
 export default class Home extends Vue {}
 </script>
