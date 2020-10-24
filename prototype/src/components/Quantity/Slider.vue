1be61d93bd07c242b5f66a285dd80daca35c9b58
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-10-24 21:58:47 +0800

    added slider

---
+++ prototype/src/components/Quantity/Slider.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card-text>
+    <v-slider
+      v-model="quantity"
+      label="Quantity"
+      :thumb-color="ex3.color"
+      thumb-label="always"
+      track-color="black"
+      color="black"
+       @change="$emit('update:quantity', quantity);"
+    ></v-slider>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  props:{
+    quantity: Number
+  },
+  data: function () {
+    return {
+      ex3: { label: "Quantity", val: 50, color: "accent"},
+    };
+  },
+  methods: {
+    sliderChange(){
+      // $emit('update:quantity', quantity);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
