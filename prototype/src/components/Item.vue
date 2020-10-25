699578ba5eb7c5292c489c810ba42f61eac835ed
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-10-25 19:47:39 +0800

    update Item View

--- prototype/src/components/Item.vue
+++ prototype/src/components/Item.vue
@@ -1,36 +1,42 @@
 <template>
-  <v-card class="item">
-    <v-img width="480px" height="360px" :src="ItemList[id].imageSource"></v-img>
-    <div class="item-content">
-      <v-card-title> {{ ItemList[id].name }} </v-card-title>
-      <v-card-text> $ {{ ItemList[id].price }} </v-card-text>
-      <v-card-text>
-        <p>Special Request?</p>
+  <div align="center" class="spacing">
+    <v-card class="item">
+      <v-img
+        width="480px"
+        height="360px"
+        :src="ItemList[id].imageSource"
+      ></v-img>
+      <div class="item-content">
+        <v-card-title> {{ ItemList[id].name }} </v-card-title>
+        <v-card-text> $ {{ ItemList[id].price }} </v-card-text>
+        <v-card-text>
+          <p>Special Request?</p>
 
-        <hr />
-      </v-card-text>
-      <v-card-text>
-        <v-icon color="accent">mdi-plus</v-icon> Add them here. We'll do our
-        best to make it happen</v-card-text
-      >
+          <hr />
+        </v-card-text>
+        <v-card-text>
+          <v-icon color="accent">mdi-plus</v-icon> Add them here. We'll do our
+          best to make it happen</v-card-text
+        >
 
-      <!-- <v-card-text> Quantity: {{ quantity }} </v-card-text> -->
+        <!-- <v-card-text> Quantity: {{ quantity }} </v-card-text> -->
 
-      <Slider v-if="type === 'slider'" :quantity.sync="quantity"></Slider>
-      <InputBox
-        v-else-if="type === 'input'"
-        :quantity.sync="quantity"
-      ></InputBox>
-      <Increment
-        v-else-if="type === 'increment'"
-        :quantity.sync="quantity"
-      ></Increment>
-      <v-btn color="accent" class="item-button" block tile @click="addToCart">
-        <v-icon class="icon-space">mdi-plus</v-icon> Add to My Order
-      </v-btn>
-      <br />
-    </div>
-  </v-card>
+        <Slider v-if="type === 'slider'" :quantity.sync="quantity"></Slider>
+        <InputBox
+          v-else-if="type === 'input'"
+          :quantity.sync="quantity"
+        ></InputBox>
+        <Increment
+          v-else-if="type === 'increment'"
+          :quantity.sync="quantity"
+        ></Increment>
+        <v-btn color="accent" class="item-button" block tile @click="addToCart">
+          <v-icon class="icon-space">mdi-plus</v-icon> Add to My Order
+        </v-btn>
+        <br />
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -41,7 +47,7 @@ import Increment from "./Quantity/Increment";
 // import VueRouter from 'vue-router'
 // import { routes } from "@/router/index.js";
 
-import ItemListJson from "@/store/Items.json"
+import ItemListJson from "@/store/Items.json";
 
 export default {
   props: {
@@ -58,12 +64,14 @@ export default {
       //   itemPrice: 99.0,
       //   quantity: 30,
       // id: 1,
-      ItemList: ItemListJson
+      ItemList: ItemListJson,
     };
   },
   methods: {
     addToCart() {
       console.log("Added to cart");
+      // TODO: add logic to update State, which in turn update cart
+
       // this.$router.push("/");
       this.$router.go(-1);
     },
@@ -91,5 +99,9 @@ export default {
 .icon-space {
   padding-left: 5px;
   padding-right: 5px;
+}
+
+.spacing{
+  padding: 20px
 }
 </style>
