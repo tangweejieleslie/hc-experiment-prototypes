65e20234914c6ca53b8787e4fb446d75206179b7
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-10-29 10:07:00 +0800

    added a landing page to select input type (for development purpose)

---
+++ prototype/src/views/SelectInputType.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>
+      <v-container>
+          <v-row>
+              <v-col>
+                  <v-btn :to="`/accordion/increment`" 
+
+                  >Increment</v-btn>
+              </v-col>
+          </v-row>
+      </v-container>
+  </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
