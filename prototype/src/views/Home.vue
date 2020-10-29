0bf114419c8226a98ece2544900ce44d8f96d340
Leslie Tang <49802619+tangweejieleslie@users.noreply.github.com>
2020-10-29 17:11:35 +0800

    Added selector for different input type

--- prototype/src/views/Home.vue
+++ prototype/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <h1>This is Group 7 UI Prototype</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <h1>This is Group 7 UI Prototype</h1>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
-  components: {
-
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
--- prototype/src/views/SelectInputType.vue
+++ prototype/src/views/SelectInputType.vue
@@ -1,23 +1,27 @@
 <template>
   <h1>
-      <v-container>
-          <v-row>
-              <v-col>
-                  <v-btn :to="`/accordion/increment`" 
-
-                  >Increment</v-btn>
-              </v-col>
-          </v-row>
-      </v-container>
+    <v-container>
+        <v-row>
+            <h1>DV2: Input Type</h1>
+        </v-row>
+      <v-row>
+        <v-col align="center">
+          <v-btn :to="`/accordion/increment`">Increment</v-btn>
+        </v-col>
+        <v-col align="center">
+          <v-btn :to="`/accordion/input`">Input</v-btn>
+        </v-col>
+        <v-col align="center">
+          <v-btn :to="`/accordion/slider`">Slider</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </h1>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
